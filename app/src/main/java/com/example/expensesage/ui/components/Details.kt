1026ad2cc7e254aee53b4a7904c7f7557608dd94
LaurensDM <laurens.demaeyer@student.hogent.be>
2023-10-04 16:28:05 +0200

    Adding dialog to detail button

--- app/src/main/java/com/example/expensesage/ui/components/Details.kt
+++ app/src/main/java/com/example/expensesage/ui/components/Details.kt
@@ -1,12 +1,15 @@
 package com.example.expensesage.ui.components
 
 
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
@@ -17,6 +20,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
@@ -28,30 +32,51 @@ import com.example.expensesage.data.Expense
 @Composable
 fun Details(expense: Expense, modifier: Modifier) {
     Card(
-        colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.primaryContainer),
+        elevation = CardDefaults.cardElevation(5.dp),
         shape = MaterialTheme.shapes.large,
-        modifier = modifier.padding(16.dp)
     ) {
-            Text(
-                text = expense.expenseName,
-                style = MaterialTheme.typography.headlineMedium,
-                textAlign = TextAlign.Center,
+        Text(
+            text = "Details",
+            style = MaterialTheme.typography.headlineMedium,
+            textAlign = TextAlign.Center,
+            lineHeight = 24.sp,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+        )
+        Column(
+            modifier = Modifier.padding(start = 8.dp, end = 8.dp, bottom = 8.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(16.dp, alignment = Alignment.CenterVertically),
+        ) {
+
+            Column(
                 modifier = Modifier.fillMaxWidth(),
-                lineHeight = 24.sp,
-            )
-            Text(
-                text = expense.expense.toString(),
-                modifier = Modifier.fillMaxWidth(),
-                textAlign = TextAlign.Center,
-                lineHeight = 12.sp,
-            )
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.spacedBy(
+                    space = 8.dp,
+                    alignment = Alignment.Top
+                ),
+            ) {
+                Text(
+                    text = "Topic: ${expense.expenseName}",
+                    style = MaterialTheme.typography.labelLarge,
+                    textAlign = TextAlign.Center,
+                )
+                Text(
+                    text = "Price: € ${expense.expense}",
+                    style = MaterialTheme.typography.labelLarge,
+                    textAlign = TextAlign.Center,
+                )
+            }
+
             Row(
+                modifier = Modifier.padding(top = 16.dp),
                 horizontalArrangement = Arrangement.SpaceEvenly,
-                modifier = Modifier.fillMaxWidth()
+                verticalAlignment = Alignment.Bottom,
             ) {
                 Button(
-                    onClick = { /*TODO*/ },
-                    colors = ButtonDefaults.buttonColors(
+                    onClick = { /*TODO*/ }, colors = ButtonDefaults.buttonColors(
                         containerColor = MaterialTheme.colorScheme.errorContainer,
                         contentColor = MaterialTheme.colorScheme.onErrorContainer
                     )
@@ -60,8 +85,7 @@ fun Details(expense: Expense, modifier: Modifier) {
                 }
                 if (expense.owed) {
                     Button(
-                        onClick = { /*TODO*/ },
-                        colors = ButtonDefaults.buttonColors(
+                        onClick = { /*TODO*/ }, colors = ButtonDefaults.buttonColors(
                             containerColor = MaterialTheme.colorScheme.primaryContainer,
                             contentColor = MaterialTheme.colorScheme.onPrimaryContainer
                         )
@@ -72,3 +96,4 @@ fun Details(expense: Expense, modifier: Modifier) {
             }
         }
     }
+}
